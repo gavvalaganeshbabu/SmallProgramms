188d2c717905009a7a65846f5b7de12f84ec711b
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-21 23:24:18 +0530

    Firstr

---
+++ Ganesh.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ganesh{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n= sc.nextInt();
+String s=isEvenOdd(n);
+System.out.println(s);
+}
+public static String isEvenOdd(int n){
+if(n%2==0)
+return "Even";
+else 
+return "Odd";
+}
+}
